57d844f2e8415f60092bb113572b88111b8314f6
SriHarsha-duppala <129260957+SriHarsha-duppala@users.noreply.github.com>
2025-09-15 06:43:55 +0530

    Update PracticeexamApplication.java

--- src/main/java/com/example/practiceexam/PracticeexamApplication.java
+++ src/main/java/com/example/practiceexam/PracticeexamApplication.java
@@ -19,7 +19,7 @@ public class PracticeexamApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://sriharsha-practice-test.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
