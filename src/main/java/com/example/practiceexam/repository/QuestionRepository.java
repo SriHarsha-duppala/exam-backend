0032e9c1093fd265650d0fd40b3a06a42ef567a2
SriHarsha-duppala <129260957+SriHarsha-duppala@users.noreply.github.com>
2025-09-16 14:31:47 +0530

    Update QuestionRepository.java

--- src/main/java/com/example/practiceexam/repository/QuestionRepository.java
+++ src/main/java/com/example/practiceexam/repository/QuestionRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findByExamId(Long examId);
+    List<Question> findByExam_Id(Long examId); // Matches entity relation
+    // saveAll() is inherited from JpaRepository
 }
